f245666c57fd39a94575d6bdc605b2016604a8e2
Rob Anderson <rob.anderson@iota.co.nz>
2013-10-05 10:34:32 +1300

    Renames controllers.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Main app config and initialisation
-
 //! app
 var app = angular.module('myApp', [
     'ng',
@@ -15,33 +13,30 @@ var app = angular.module('myApp', [
 
 //! config
 app.config(function($routeProvider, $locationProvider, $httpProvider) {
-
+    
     $routeProvider
-        .when('/',                  { templateUrl: 'partials/index',        controller: 'IndexController' })
-        .when('/users',             { templateUrl: 'partials/users',        controller: 'UserController' })
-        .when('/user/create',       { templateUrl: 'partials/user-create',  controller: 'UserController' })
-        .when('/user/view/:id',     { templateUrl: 'partials/user-view',    controller: 'UserController' })
-        .when('/user/edit/:id',     { templateUrl: 'partials/user-edit',    controller: 'UserController' })
-        .when('/user/delete/:id',   { templateUrl: 'partials/users',        controller: 'UserController' })        
-        .otherwise(                 { templateUrl: 'partials/404',          controller: '404Controller', title: '404'});
+        .when('/',                  { templateUrl: 'partials/index',        controller: 'IndexCtrl' })
+        .when('/users',             { templateUrl: 'partials/users',        controller: 'UserCtrl' })
+        .when('/user/create',       { templateUrl: 'partials/user-create',  controller: 'UserCtrl' })
+        .when('/user/view/:id',     { templateUrl: 'partials/user-view',    controller: 'UserCtrl' })
+        .when('/user/edit/:id',     { templateUrl: 'partials/user-edit',    controller: 'UserCtrl' })
+        .when('/user/delete/:id',   { templateUrl: 'partials/users',        controller: 'UserCtrl' })        
+        .otherwise(                 { templateUrl: 'partials/404',          controller: '404Ctrl', title: '404'});
         
     // Removes the # in urls
     $locationProvider.html5Mode(true);   
     
     // Intercept responses with a handler service
-    $httpProvider.responseInterceptors.push('responseHandler');         
-    
+    $httpProvider.responseInterceptors.push('responseHandler');     
 });
 
 
 //! run
-app.run(function($rootScope, crResources) {
-   
-    $rootScope.options = {
-        path: '/cores'
-    };
+app.run(function(crResources) {
+       
+    var options = { path: '/cores' };
     
-    crResources.init($rootScope.options).then(function() {
+    crResources.init(options).then(function() {
     
         //console.log('cores initialized');                   
     });     
